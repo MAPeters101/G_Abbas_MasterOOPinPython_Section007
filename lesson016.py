c37ff6ac61a4a6a12426868a98d7d3c6f4ffe22e
Mark Peters <mapeters101@earthlink.net>
2025-05-26 14:41:57 -0400

    Complete work on lesson016 from 6:15.

--- lesson016.py
+++ lesson016.py
@@ -2,7 +2,18 @@ class Employee:
     name = "Paul" # Public member
     _age = 30 # Protected member
     __salary = 5000 # Private member
+        # _Employee__salary
+
+class Test(Employee):
+    def showData(self):
+        print("Age: ", self._age)
 
 
-paul = Employee()
+paul = Test()
 print(paul.name)
+paul.showData()
+#print(paul.__salary)
+employee = Employee()
+#print(employee.__salary)
+print(employee._Employee__salary)
+
